168bde90f00481fee2ca646741c44f24b0443c52
KhushiJain1011 <khushij1011@gmail.com>
2026-09-16 10:50:12 +0530

    frontend url - cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "meeting-room-booking-python-bxerlowlp.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
